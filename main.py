e35baaf2c29c8b2fd141f2862cd2ddc9a8fd3767
Llhama <121739691+shamahamichoh@users.noreply.github.com>
2023-01-04 17:10:34 +0000

    menue

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 def take_order():
   name = input("Hello welcome to order what should we call you")
-  order = input("what would you like to order today" + name + " Choose from menue.")
-  print("your order " + order + "has been sent to the kitchen")
+  order = input("what would you like to order today " + name + " Choose from menue.")
+  print("your order " + order + " has been sent to the kitchen")
 
 def menue():
   print("our menue is being displayed now")
@@ -43,29 +43,41 @@ def specials():
 def make_reservration():
   table = input("what table would you like to reserve a seat in")
   print("allright we will reserve your seat at table" + table)
-  
+
+
+
 def welcome():
     print("""
 Welcome to out resturant chatbot I will offer you assistance with
 any of your orders and any extra questions you would like to ask
-""")
-    choice = input("""
+ """)
+    choices = input("""
+    would you like to begin yes to yes or q to quit
+    """)
+    
+    quit_character = "q"
+    while choices.lower != quit_character:
+      choice = input("""
     What do you want me to help you with
     1 Menue
     2 Todays Specails
     3 Make a reservration
     4 order
     """)
-    if choice == "1":
-      menue()
-    elif choice == "2":
-      specials()
-    elif choice == "3":
-      make_reservration 
-    elif choice == "4":
-      take_order()
-    else:
-      print("choose type in a number from the choices listed")
+      
+      if choice == quit_character:
+        break
+      elif choice == "1":
+        menue()
+      elif choice == "2":
+        specials()
+      elif choice == "3":
+        make_reservration 
+      elif choice == "4":
+        take_order()
+        break
+      else:
+        print("choose type in a number from the choices listed")
 
 welcome()      
     
